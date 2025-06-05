4d272f020f4d1ca960d80ba7d9a178ebce5cac35
kennykenny <gbmkocc1@gmail.com>
2025-06-05 09:59:26 +0100

    Initial commit with Terraform files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# eks-infra/variables.tf
+variable "region" {
+  default = "eu-north-1"
+}
+
+variable "ec2_key_name" {
+  default = "github-keypair"
+  type        = string
+}
+
+variable "my_ip" {
+  default = "188.31.175.209/32"
+  type        = string
+}
